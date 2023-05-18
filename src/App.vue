c39949f024a33bb418ee130290c921e8865dc91d
Extraordinary <732487614@qq.com>
2023-05-18 10:19:29 +0800

    add: emelemt-plus

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>Extraordianry</h1>
+    <el-button type="info" circle :icon="Edit">button</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { Edit } from '@element-plus/icons-vue'
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入 element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
